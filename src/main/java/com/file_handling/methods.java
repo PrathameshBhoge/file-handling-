6a2187e1014203756faafd5e003f937b69d22aa2
Prathamesh Bhoge <Prathameshbhoge.cool@gmail.com>
2024-01-03 15:15:45 +0530

    methods to handel the file

--- src/main/java/com/file_handling/methods.java
+++ src/main/java/com/file_handling/methods.java
@@ -5,11 +5,17 @@ import java.io.File;
 public class methods {
     public static void main(String[] args) {
         try {
-            File file = new File("C:/Users/91749/OneDrive/Documents/Project Clgg/CyberChat.docx");
+            File file = new File("C:/Users/91749/OneDrive/Documents/Project Clgg");
             if(file.exists()){
                 System.out.println("File found");
                 System.out.println(file.canRead());
                 System.out.println(file.canWrite());
+                System.out.println(file.isFile());
+                System.out.println(file.isDirectory());
+               File f[] = file.listFiles();
+               for(int i=0;i<f.length;i++){
+                   System.out.println(f[i]);
+               }
             }
             else {
                 System.out.println("File not found");
@@ -20,6 +26,9 @@ public class methods {
         }
     }
 }
-//exists()
-//canRead()
-//canWrite()
+//exists() -- it checks whether your file exixts or not
+//canRead() -- it checks whether your file is readbele or not
+//canWrite() -- it checks whether your file is writable  or not
+//isfile()
+//isDirectory()
+//listFiles()
