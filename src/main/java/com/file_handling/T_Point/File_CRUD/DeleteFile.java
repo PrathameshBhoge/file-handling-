cef40c4a2c21ecb5ebc3fba7b8335c7d7c74d9c7
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-05-10 19:59:21 +0530

    initial commit

--- src/main/java/com/file_handling/T_Point/File_CRUD/DeleteFile.java
+++ src/main/java/com/file_handling/T_Point/File_CRUD/DeleteFile.java
@@ -3,6 +3,7 @@ package com.file_handling.T_Point.File_CRUD;
 import java.io.File;
 
 public class DeleteFile {
+
     public static void main(String[] args) {
         File file = new File("NewFile.txt");
         if(file.delete()){
